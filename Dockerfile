ea7b067f7d1131051cf3d0fd4c871bda485ace1b
David J Felix <felix.davidj@gmail.com>
2017-05-31 11:26:08 -0400

    Convert to using ffmpeg; finish command line

--- Dockerfile
+++ Dockerfile
@@ -8,13 +8,17 @@ RUN apt-get -yq update && \
     && \
     echo "deb https://packages.cloud.google.com/apt gcsfuse-jessie main" \
     > /etc/apt/sources.list.d/gcsfuse.list && \
+    echo "deb http://ftp.debian.org/debian jessie-backports main" \
+    > /etc/apt/sources.list.d/backports.list && \
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \
     | apt-key add - && \
     apt-get -yq update && \
     apt-get -yq install --no-install-recommends \
-        libav-tools \
         kmod \
         gcsfuse \
+    && \
+    apt-get -yqt jessie-backports  install --no-install-recommends \
+        ffmpeg \
     && \
     rm -rf /var/lib/apt/lists/*
 # </2
@@ -32,12 +36,10 @@ RUN go build github.com/iotv/missouri
 
 # Setup google cloud storage mounts
 ENV GOOGLE_APPLICATION_CREDENTIALS=/go/src/github.com/iotv/missouri/iotv-Raw-Videos-Service-Admin.json
-RUN mkdir /data && \
-    gcsfuse iotv-1e541.appspot.com /data
-
+RUN mkdir /data
+CMD ./run
 
 # 1 FROM debian
 
 # 3 WORKDIR /opt/
 # 4 COPY --from=0 /go/src/github.com/iotv/missouri/missouri .
-# 5 CMD ["./missouri"]
--- missouri.go
+++ missouri.go
@@ -1,16 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"cloud.google.com/go/pubsub"
+	"encoding/json"
+	"fmt"
 	"golang.org/x/net/context"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
 )
+
+type TranscodeRawVideoMessage struct {
+	Name string `json:"name"`
+}
+
+type RawVideoCommand struct {
+	InputFile string
+	OutputDir string
+}
 
 func echoMsg(ctx context.Context, msg *pubsub.Message) {
 	msg.Ack()
 	fmt.Printf("Msg: %v\n", msg.ID)
 	fmt.Printf("Data: %s\n", string(msg.Data))
+
+	var trvMsg TranscodeRawVideoMessage
+	err := json.Unmarshal(msg.Data, &trvMsg)
+	if err != nil {
+		log.Fatalf("Unmarshal error: %v", err)
+	}
+
+	rvc := RawVideoCommand{
+		InputFile: strings.TrimPrefix(trvMsg.Name, "raw-videos/"),
+		OutputDir: strings.Split(strings.TrimPrefix(trvMsg.Name, "raw-videos/"), "/")[1],
+	}
+
+	// FIXME: this is fairly unsafe. If the path can be manipulated this could jump to the container fs
+	cmdArgs := fmt.Sprintf("-nostdin -i /data/raw-videos/%s -hls_time 2 -hls_list_size 0 -f hls -hls_segment_filename /data/videos/%s/%%16d /data/videos/%s/index.m3u8", rvc.InputFile, rvc.OutputDir, rvc.OutputDir)
+	fmt.Println(cmdArgs)
+	os.MkdirAll("/data/videos/" +rvc.OutputDir, 0777)
+	cmd := exec.Command("ffmpeg", strings.Split(cmdArgs, " ")...)
+	err = cmd.Run()
+
+	if err != nil {
+		fmt.Printf("Command Error: %v", err)
+	}
 }
 
 func main() {
@@ -23,10 +58,12 @@ func main() {
 	}
 
 	subscription := client.Subscription("transcode-raw-videos")
+	exists, err := subscription.Exists(ctx)
 
 	if err != nil {
 		log.Fatalf("Exists error: %v", err)
 	}
+	fmt.Printf("Subscription Exists: %v\n", exists)
 
 	for {
 		subscription.Receive(ctx, echoMsg)
